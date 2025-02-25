38871c16d1bb9184a1859c2d0134bcd0a73eae21
profmanjupriya <mmuralidharanpriya@sdsu.edu>
2025-02-25 01:33:00 -0800

    updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'public/index.html',
+      input: 'index.html',
     },
   },
   css: {
@@ -25,4 +25,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
