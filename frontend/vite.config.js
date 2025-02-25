37fc7edec63b3c972ba96af718d5a44048884956
profmanjupriya <mmuralidharanpriya@sdsu.edu>
2025-02-25 01:08:45 -0800

    Connect frontend to deployed backend 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  publicDir: 'public',
   build: {
-    outDir: 'dist', // Ensure the output directory is set correctly
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html',
+    },
   },
   server: {
     proxy: {
